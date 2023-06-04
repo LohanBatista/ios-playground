a7e0c41121702a6c3d941c497a3f0969c9bca979
Lohan Batista <lohan.batista@digitalbusiness.com>
2023-06-04 20:04:42 -0300

    first commit

--- 01.playground/Contents.swift
+++ 01.playground/Contents.swift
@@ -1,3 +1,48 @@
-import UIKit
+let nome: String = "Lohan" // no swift LET é uma constante
+var idade: Int = 26
+print("Meu nome é \(nome) e tenho atualmente \(idade) anos")
 
-var greeting = "Hello, playground"
+var coeficienteArmazenamento: Double = 8.6
+var possuiHistoricoReprovacao: Bool = false
+var turnoEmQueEstuda: Character = "D" //quando utilizamos somente um caracter utilizamos o tipo CHARACTER
+
+typealias Estudante = String // segue como se fosse Typescript
+
+var aluno1: Estudante = "João"
+var aluno2: Estudante = "Maria"
+
+//=====================================\\
+
+var inicialNome: Character = "L"
+var nomeAluno: String = "Lohan"
+var apelido: String = "Lolo"
+var idadeAluno: Int = 26
+var altura: Double = 1.79
+var gostaDeBeber: Bool = true
+
+print(
+    "Inicial do nome: \(inicialNome)",
+    "Nome: \(nomeAluno)",
+    "Apelido: \(apelido)",
+    "Idade: \(idadeAluno)",
+    "Altura: \(altura)",
+    "Gosta de beber ? : \(gostaDeBeber)"
+    )
+
+
+//=====================================\\
+
+
+var tempCelcius: Double = 10
+let tempFahrenheit = (tempCelcius * 9/5) + 32
+
+print("Em fahrenheit, a temperatura é… \(tempFahrenheit)")
+
+
+let nota01: Double = 8.0
+let nota02: Double = 7.5
+let nota03: Double = 9.0
+let mediaDasNotas = (nota01 + nota02 + nota03) / 3
+print("A média das notas é \(mediaDasNotas).")
+
+//=====================================\\
